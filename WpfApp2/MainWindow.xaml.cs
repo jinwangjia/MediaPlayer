9077d65088a2fffe6f9700a4d509389d2e8e8725
wangjia <jin_wangjia@163.com>
2019-10-12 16:24:05 +0800

    添加项目文件。

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer player = new MediaPlayer();
+            player.Open(new Uri($"{AppContext.BaseDirectory}\\Resources\\8773.mp3", UriKind.RelativeOrAbsolute));
+            player.Play();
+        }
+    }
+}
